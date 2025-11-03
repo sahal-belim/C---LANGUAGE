cee58ad4d5d1ee94694279350cddc89f4b1071b5
Sahal Belim <sahalbelim25@gmail.com>
2025-11-03 17:54:22 +0530

    Add stack implementation using array in C
    Implement a stack using an array with push, pop, and display functions.

---
+++ stack_using_array.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define SIZE 5
+
+int stack[SIZE], top = -1;
+
+void push(int value) {
+    if (top == SIZE - 1)
+        printf("Stack Overflow\n");
+    else {
+        stack[++top] = value;
+        printf("%d pushed to stack\n", value);
+    }
+}
+
+void pop() {
+    if (top == -1)
+        printf("Stack Underflow\n");
+    else
+        printf("%d popped from stack\n", stack[top--]);
+}
+
+void display() {
+    if (top == -1)
+        printf("Stack is empty\n");
+    else {
+        printf("Stack elements:\n");
+        for (int i = top; i >= 0; i--)
+            printf("%d\n", stack[i]);
+    }
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    display();
+    pop();
+    display();
+
+    return 0;
+}
