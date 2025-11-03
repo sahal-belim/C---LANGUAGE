4672d9e30861665bdab6d1b396d6e1ba2f632e50
Sahal Belim <sahalbelim25@gmail.com>
2025-11-03 17:48:51 +0530

    Add string reversal program in reverse_string.c
    Implement a program to reverse a string input by the user.

---
+++ reverse_string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    int len = strlen(str) - 1;
+    printf("Reversed string: ");
+    for (int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
